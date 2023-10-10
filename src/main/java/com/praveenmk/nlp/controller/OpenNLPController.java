00e16fde313a2885e1e0eb434d68f3839a5cf437
Praveen MK <praveen.mk@outlook.com>
2023-10-10 10:17:02 +0000

    Added tokennizer and more training data

--- src/main/java/com/praveenmk/nlp/controller/OpenNLPController.java
+++ src/main/java/com/praveenmk/nlp/controller/OpenNLPController.java
@@ -18,6 +18,11 @@ import opennlp.tools.doccat.DocumentCategorizerME;
 import opennlp.tools.doccat.DocumentSample;
 import opennlp.tools.doccat.DocumentSampleStream;
 import opennlp.tools.doccat.FeatureGenerator;
+import opennlp.tools.tokenize.TokenSample;
+import opennlp.tools.tokenize.TokenSampleStream;
+import opennlp.tools.tokenize.TokenizerFactory;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.InputStreamFactory;
 import opennlp.tools.util.MarkableFileInputStreamFactory;
 import opennlp.tools.util.ObjectStream;
@@ -31,11 +36,27 @@ public class OpenNLPController {
 	@GetMapping(value = "/train-model")
 	public String getTrainModel() throws IOException {
 
-		// Read file with classifications samples of sentences.
-		InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(
-				new File("commodityCategoryData.txt"));
+		/**
+		 * Read human understandable data & train a model
+		 */
+		// Read file with examples of tokenization.
+		InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("tokenizerdata.txt"));
 		ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
-		ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
+		ObjectStream<TokenSample> sampleStream = new TokenSampleStream(lineStream);
+ 
+		// Train a model from the file read above
+		TokenizerFactory factory = new TokenizerFactory("en", null, false, null);
+		TokenizerModel model = TokenizerME.train(sampleStream, factory, TrainingParameters.defaultParams());
+ 
+		// Serialize model to some file so that next time we don't have to again train a
+		// model. Next time We can just load this file directly into model.
+		model.serialize(new File("tokenizermodel.bin"));
+
+		// Read file with classifications samples of sentences.
+		inputStreamFactory = new MarkableFileInputStreamFactory(
+				new File("commodityCategoryData.txt"));
+		lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
+		ObjectStream<DocumentSample> docSampleStream = new DocumentSampleStream(lineStream);
 
 		// Use CUT_OFF as zero since we will use very few samples.
 		// BagOfWordsFeatureGenerator will treat each word as a feature. Since we have
@@ -43,14 +64,14 @@ public class OpenNLPController {
 		// cutoff.
 		TrainingParameters params = ModelUtil.createDefaultTrainingParameters();
 		params.put(TrainingParameters.CUTOFF_PARAM, 0);
-		DoccatFactory factory = new DoccatFactory(new FeatureGenerator[] { new BagOfWordsFeatureGenerator() });
+		DoccatFactory docCatFactory = new DoccatFactory(new FeatureGenerator[] { new BagOfWordsFeatureGenerator() });
 
 		// Train a model with classifications from above file.
-		DoccatModel model = DocumentCategorizerME.train("en", sampleStream, params, factory);
+		DoccatModel docCatModel = DocumentCategorizerME.train("en", docSampleStream, params, docCatFactory);
 
 		// Serialize model to some file so that next time we don't have to again train a
 		// model. Next time We can just load this file directly into model.
-		model.serialize(new File("documentcategorizer.bin"));
+		docCatModel.serialize(new File("documentcategorizer.bin"));
 
 		return new String();
 	}
@@ -71,13 +92,9 @@ public class OpenNLPController {
 			// Initialize document categorizer tool
 			DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
 
-			for (String st : shipmentDesc.split(";")) {
-				System.out.println(st);
-			}
-
 			// Get the probabilities of all outcome i.e. positive & negative
-			double[] probabilitiesOfOutcomes = myCategorizer.categorize(shipmentDesc.split(";"));
-
+			double[] probabilitiesOfOutcomes = myCategorizer.categorize(getTokens(shipmentDesc));
+			System.out.println("probabilitiesOfOutcomes : " + Arrays.toString(probabilitiesOfOutcomes));
 			// Get name of category which had high probability
 			category = myCategorizer.getBestCategory(probabilitiesOfOutcomes);
 			System.out.println("Category: " + category);
@@ -89,4 +106,30 @@ public class OpenNLPController {
 		return shipmentDesc + " [ " + category + "]";
 	}
 
+
+	/**
+	 * Tokenize sentence into tokens.
+	 * 
+	 * @param sentence
+	 * @return
+	 */
+	private static String[] getTokens(String sentence) {
+ 
+		// Use model that was created in earlier tokenizer tutorial
+		try (InputStream modelIn = new FileInputStream("tokenizermodel.bin")) {
+ 
+			TokenizerME myCategorizer = new TokenizerME(new TokenizerModel(modelIn));
+ 
+			String[] tokens = myCategorizer.tokenize(sentence);
+ 
+			for (String t : tokens) {
+				System.out.println("Tokens: " + t);
+			}
+			return tokens;
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
